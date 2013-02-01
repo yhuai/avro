91184792b3e78f2f4e1f1031f20b80a8671bbfb8
yhuai <huaiyin.thu@gmail.com>
2013-02-01 17:39:48 -0500

    check invalid parameter

--- lang/java/trevni/core/src/main/java/org/apache/trevni/ColumnValues.java
+++ lang/java/trevni/core/src/main/java/org/apache/trevni/ColumnValues.java
@@ -186,6 +186,9 @@ public class ColumnValues<T extends Comparable>
     if (row >= column.lastRow(block)) {
       if (block >= column.blockCount())
         throw new TrevniRuntimeException("Read past end of column.");
+      if (numOfPrefetched < 0)
+        throw new TrevniRuntimeException("The number of prefetched blocks should " +
+        		"be equal or greater than 0");
       startBlockWithPrefetch(block+1, numOfPrefetched);
     }
     row++;
